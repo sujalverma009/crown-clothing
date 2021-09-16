d55b87244adf8b3cd6ae2634c835a548d7102da1
Sujal Verma <sujalverma504@gmail.com>
2021-09-16 14:03:20 +0530

    update our menu-items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,33 +12,33 @@ this.state = {
     title: 'Hats',
     imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     id: 1,
-    linkUrl: 'shop/hats'
+    linkUrl: 'hats'
   },
   {
     title: 'Jackets',
     imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     id: 2,
-    linkUrl: 'shop/jackets'
+    linkUrl: ''
   },
   {
     title: 'Sneakers',
     imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     id: 3,
-    linkUrl: 'shop/sneakers'
+    linkUrl: ''
   },
   {
     title: 'Womens',
     imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     size: 'large',
     id: 4,
-    linkUrl: 'shop/womens'
+    linkUrl: ''
   },
   {
     title: 'Mens',
     imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     size: 'large',
     id: 5,
-    linkUrl: 'shop/mens'
+    linkUrl: ''
   }
 ]
 
@@ -49,8 +49,8 @@ this.state = {
  render(){
      return(
          <div className='directory-menu'>
-             {this.state.sections.map(({title,imageUrl,id,size})=>(
-                 <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+             {this.state.sections.map(({id,...otherSectionProps})=>(
+                 <MenuItem key={id} {...otherSectionProps} />
              ))}
          </div>
      )
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({title,imageUrl,size}) => {
+const MenuItem = ({title,imageUrl,size,history,linkUrl,match}) => {
     return (
         <div
-         className={`${size} menu-item`}>
+         className={`${size} menu-item`} onClick={()=> history.push(`${match.url}${linkUrl}`)} >
 
         <div className='background-image'  style={{
             backgroundImage:`url(${imageUrl})`}}
@@ -18,4 +19,4 @@ const MenuItem = ({title,imageUrl,size}) => {
     )
 }
 
-export default MenuItem
+export default withRouter(MenuItem);
